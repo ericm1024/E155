e97a13fb3fa8edb34f8f28bfb4258e67874849aa
Eric Mueller <eric.mueller1024@gmail.com>
2015-10-21 14:50:54 -0700

    lab6: change backend to use internal buffer and signals instead of fifo
    Not tested because OSX.

--- lab6/adc_read.c
+++ lab6/adc_read.c
@@ -12,15 +12,22 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/prctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
+/**
+ * \brief read a single 10-bit value from the adc via serial.
+ * \return The value read
+ * \pre Assumes the spi peripheral address range has been mapped
+ */
 static unsigned adc_read()
 {
         unsigned dval;
@@ -54,49 +61,99 @@ static unsigned adc_read()
 }
 
 #define BUFF_FNAME "/tmp/adc_read_buffer"
+#define WWW_DATA_UID 1001
+#define BUFFSIZE 1000
 
-static void eat_lines(int fd, unsigned nr_lines)
+static unsigned buffer[BUFFSIZE] = {0};
+static unsigned buff_idx = 0;
+
+/**
+ * \brief Dump the contents of the buffer to the output file.
+ */
+static void dump_buffer(int unused)
 {
-        unsigned i;
-        char c;
+	unsigned count;
+	unsigned idx = buff_idx + 1;
+	int fd;
 
-        for (i = 0; i < nr_lines; ++i) {
-                while (read(fd, &c, 1) == 0)
-                        if (c == '\n')
-                                break;
-                printf("ate a line, om nom nom\n");
-        }
+	fd = open(BUFF_FNAME, O_CREAT|O_TRUNC|O_RDWR, 00644);
+	if (fd < 0) {
+		printf("%s: %s\n", __func__, strerror(errno));
+		exit(1);
+	}
+
+	for (count = 1; count < BUFFSIZE; ++count)
+		dprintf(fd, "%d\n", buffer[idx++ % BUFFSIZE]);
+
+	close(fd);
+}
+
+/**
+ * \brief Read from the file descriptor and write out to a file in
+ * tmpfs when signaled
+ * \param read_fd   The read end of a pipe
+ */
+static void reader_loop(int read_fd)
+{
+	int ret;
+	sighandler_t sh;
+
+	/*
+	 * drop root privs -- we need to be the www-data user so that we can
+	 * get signaled
+	 */
+	ret = setuid(WWW_DATA_UID);
+	if (ret) {
+		printf("%s: %s\n", __func__, strerror(errno));
+		exit(2);
+	}
+
+	/* rename ourselves so the cgi-script can find our pid by name */
+	ret = prctl(PR_SET_NAME, "adc-buffer", 0, 0, 0);
+	if (ret) {
+		printf("%s: %s\n", __func__, strerror(errno));
+		exit(3);
+	}
+
+	/* set up signal handler */
+	sh = signal(SIGUSR1, dump_buffer);
+	if (sh == SIG_ERR) {
+		printf("%s: %s\n", __func__, strerror(errno));
+		exit(3);
+	}
+
+	for (;;) {
+		unsigned val;		
+		ret = read(read_fd, &val, sizeof val);
+		assert(ret == 4);
+		buffer[buff_idx++ % BUFFSIZE] = val;
+	}
 }
 
 int main()
 {
-	int ret, fd;
+	int ret, read_fd, write_fd, fds[2];
+	pid_t pid;
         struct timespec ts = {.tv_sec = 1, .tv_nsec = 1000*1000};
 
 	ret = pi_mem_setup();
 	if (ret)
 		exit(1);
-        
-        ret = mknod(BUFF_FNAME, S_IFIFO|00644, 0);
-        if (ret && errno != EEXIST) {
-                printf("%s\n", strerror(errno));
-                exit(2);
-        }
-                
-        fd = open(BUFF_FNAME, O_RDWR);
-        if (fd < 0)
-                exit(3);
-        
-        for (;;) {
-                ret = dprintf(fd, "%d\n", adc_read());
+	
+	ret = pipe(fds);
+        if (ret)
+		exit(1);
 
-                if (ret < 0) {
-                        assert(errno == EAGAIN);
-                        eat_lines(fd, 20);
-                }
+	read_fd = fds[0];
+	write_fd = fds[1];
 
-                nanosleep(&ts, NULL);
-        }
-        
+	pid = fork();
+	if (pid < 0)
+		exit(1);
+	else if (!pid)
+		reader_loop(read_fd);
+	else
+		writer_loop(write_fd);
+
 	return 0;
 }
