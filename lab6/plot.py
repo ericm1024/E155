0006d0b3f0f81e4a823663131cc680f361f7802d
Eric Mueller <eric.mueller1024@gmail.com>
2015-10-22 03:14:16 -0700

    lab6: make plot.py suck less
    Remove debugging log file, make units of plot make sense, use
    a better title, etc.

--- lab6/plot.py
+++ lab6/plot.py
@@ -13,42 +13,38 @@
 # Torn appart and addapted in a sleep deprived rage for E155 lab6 on
 # October 21 2015
 
+SAMPLE_PERIOD_MS=10
+
 import sys, os, time, matplotlib, signal
 from subprocess import check_output
 # Tell matplotlib to use a backend that doesn't requre an X-server
 matplotlib.use('Agg')
 import pylab as plt
 
-log=open("/tmp/plot_log", "w")
-
-log.write("A\n")
-
 pid=int(check_output(["pgrep", "adc-buffer"]))
 os.kill(pid, signal.SIGUSR1)
-
-log.write("B\n")
-
-# plot the buffer
-fig = plt.subplot(111)
-plt.title("Digital voltage readings for some time period")
-plt.xlabel("time")
-plt.ylabel("digital V");
-
-log.write("C\n")
 
 Y=[]
 with open("/tmp/adc_read_buffer", 'r') as f:
     for line in f:
         Y.append(int(line))
 
-X=range(len(Y))
-fig.scatter(X,Y)
+# do digital --> real voltage conversion. conversion from MCP3002 data sheet
+map(lambda x: x*5/1024.0, Y)
 
-log.write("D\n")
+# plot the buffer
+fig = plt.subplot(111)
+plt.title("Digital voltage readings for the last %d seconds"
+          % len(Y)*SAMPLE_PERIOD_MS/1000.0)
+plt.xlabel("time (seconds)")
+plt.ylabel("Voltage (V)");
+
+# make range of X values
+X=range(len(Y))
+map(lambda x: x*SAMPLE_PERIOD_MS/1000.0), X)
+
+fig.scatter(X,Y)
 
 # http://stackoverflow.com/a/3198124/3775803
 sys.stdout.write("Content-type: image/png\r\n\r\n")
 plt.savefig(sys.stdout)
-#plt.savefig("plot.png")
-
-log.write("E\n")
