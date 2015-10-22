adcb3670412228eb818eccd74c487c52af5e50c2
Eric Mueller <eric.mueller1024@gmail.com>
2015-10-22 02:59:42 -0700

    lab6: clean up adc_read.c
    - improve the comments now that everything actually works, explain
      what the fuck is going on so I can figgure it out in a week
    - remove an unused include
    - remove some print statements for syscalls that will realistically
      never fail
    - remove Doxygen syntax from most comments because I'm never gonna
      dox this shit and it's just annoying to other readers otherwise
    - add a hilarious define because it's 3am
    - no one will ever read this commit message rofl

--- lab6/adc_read.c
+++ lab6/adc_read.c
@@ -1,19 +1,23 @@
 /**
  * \file adc_read.c
  * \author Eric Mueller -- emueller@hmc.edu
+ * 
  * \brief Use the Pi's SPI hardware to communicate with the MuddPi's adc
  * and read the 10 bit-value corresponding to the current light level.
+ *
+ * \detail This program is meant to be run as a daemon and NOT as a CGI script.
+ * TODO: doccument design/change child process bs if possible.
  */
 
 #include "../lib/libpi.h"
 
+/* we want sighandler_t from signal.h */
 #define _GNU_SOURCE
 
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,10 +27,12 @@
 #include <time.h>
 #include <unistd.h>
 
-/**
- * \brief read a single 10-bit value from the adc via serial.
- * \return The value read
- * \pre Assumes the spi peripheral address range has been mapped
+/* for hilarious infinite loops */
+#define ever ;;
+
+/*
+ * Read and return a single 10-bit value from the adc via spi.
+ * This assumes the spi peripheral address range has been mapped
  */
 static unsigned adc_read()
 {
@@ -35,18 +41,22 @@ static unsigned adc_read()
         pi_spi0_init(SPI_976kHz);
 
         /*
-	 * here we prep the adc for reading. It wants to see the following
-	 * bits on MOSI.
+	 * The ADC wants to see the following bits on MOSI:
 	 *     1. a single high bit signifying the start of communication
 	 *     2. a bit determining if the adc is in single ended or
 	 *        differential input mode. 1 for us because we are in
-	 *        single-ended mode
+	 *        single-ended mode, i.e. referencing ground.
 	 *     3. a bit selecting which channel to read from. 0 for us.
 	 *     4. a bit determing if the output should be MSB first. 1 for
-	 *        us.
+	 *        us because it's simple.
 	 *
 	 * Data is transmitted MSB-first, so those bits are in reverse order
-	 * starting at the highest bit.
+	 * starting at the highest bit, i.e. '0xD'
+	 *
+	 * Data is also transmitted/recieved in 1 byte chunks, (and there is
+	 * no clock if we're not transmitting data) so we need to send
+	 * an extra byte of all 0's in order to produce enough clocks to get
+	 * all 10 bits back from the ADC.
 	 */
         pi_spi_write(0xD << 3);
 	dval = (unsigned)pi_spi_read() << 8;
@@ -54,6 +64,7 @@ static unsigned adc_read()
         dval |= pi_spi_read();
         pi_spi_end();
 
+	/* ADC always sends a null bit before it sends the 10 data bits */
         assert((dval & 1 << 10) == 0);
         dval &= (1 << 10) - 1;
 
@@ -64,11 +75,13 @@ static unsigned adc_read()
 #define WWW_DATA_UID 33
 #define BUFFSIZE 1000
 
+/* buffer used by child process */
 static unsigned buffer[BUFFSIZE] = {0};
 static unsigned buff_idx = 0;
 
-/**
- * \brief Dump the contents of the buffer to the output file.
+/*
+ * Dump the contents of the buffer to the output file.
+ * This is called in signal context.
  */
 static void dump_buffer(int unused)
 {
@@ -90,11 +103,7 @@ static void dump_buffer(int unused)
 	close(fd);
 }
 
-/**
- * \brief Read from the file descriptor and write out to a file in
- * tmpfs when signaled
- * \param read_fd   The read end of a pipe
- */
+/* Read from the pipe and write out to a file when signaled */
 static void reader_loop(int read_fd)
 {
 	int ret;
@@ -106,42 +115,37 @@ static void reader_loop(int read_fd)
 	 * get signaled
 	 */
 	ret = setuid(WWW_DATA_UID);
-	if (ret) {
-		printf("%s: %s\n", __func__, strerror(errno));
-		exit(2);
-	}
+	if (ret)
+		exit(1);
 
 	/* rename ourselves so the cgi-script can find our pid by name */
 	ret = prctl(PR_SET_NAME, "adc-buffer", 0, 0, 0);
-	if (ret) {
-		printf("%s: %s\n", __func__, strerror(errno));
-		exit(3);
-	}
+	if (ret)
+		exit(1);
 
 	/* set up signal handler */
 	sh = signal(SIGUSR1, dump_buffer);
-	if (sh == SIG_ERR) {
-		printf("%s: %s\n", __func__, strerror(errno));
-		exit(3);
-	}
+	if (sh == SIG_ERR)
+		exit(1);
 
-	for (;;) {
+	for (ever) {
 		ret = read(read_fd, &val, sizeof val);
 		assert(ret == sizeof val);
 		buffer[buff_idx++ % BUFFSIZE] = val;
 	}
 }
 
-/**
- * \brief Poll the adc and write the results to the pipe.
- */
+#define SAMPLE_INTERVAL_MS (10)
+
+/* Poll the adc and write the results to the pipe. */
 static void writer_loop(int write_fd)
 {
-        struct timespec ts = {.tv_sec = 0, .tv_nsec = 1000*1000*10};
+        struct timespec ts = {.tv_sec = 0, 
+			      .tv_nsec = 1000*1000*SAMPLE_INTERVAL_MS};
         unsigned val;
         int ret;
 
-        for (;;) {
+        for (ever) {
                 val = adc_read();
                 ret = write(write_fd, &val, sizeof val);
                 assert(ret == sizeof val);
@@ -149,6 +153,7 @@ static void writer_loop(int write_fd)
         }
 }
 
+/* Create a pipe then fork off a child with one end of the pipe. */
 int main()
 {
 	int ret, read_fd, write_fd, fds[2];
